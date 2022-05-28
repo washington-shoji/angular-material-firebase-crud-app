ca7a04b56d3c47b0fe17a3c26e8aa51fd3484b84
washington-shoji <shoji.washington@gmail.com>
2022-05-28 15:22:35 +1000

    feat(module): Added material ng references  and custom schemas to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,19 +6,18 @@ import { AppComponent } from './app.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
 import { EditBookComponent } from './components/edit-book/edit-book.component';
 import { BookListComponent } from './components/book-list/book-list.component';
+import { AngularMaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddBookComponent,
     EditBookComponent,
-    BookListComponent
+    BookListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, AngularMaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
