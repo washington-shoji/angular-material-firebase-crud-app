221178eed79b6403511152b554a26e3ee3b8288f
washington-shoji <shoji.washington@gmail.com>
2022-05-28 17:07:17 +1000

    feat(service): Added book service to the app

---
+++ src/app/shared/book.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  booksRef!: AngularFireList<any>;
+  bookRef!: AngularFireObject<any>;
+  constructor(private db: AngularFireDatabase) {}
+
+  /* Create book */
+  AddBook(book: Book): void {
+    this.booksRef
+      .push({
+        book_name: book.book_name,
+        isbn_10: book.isbn_10,
+        author_name: book.author_name,
+        publication_date: book.publication_date,
+        binding_type: book.binding_type,
+        in_stock: book.in_stock,
+        languages: book.languages,
+      })
+      .catch((error) => {
+        this.errorMgt(error);
+      });
+  }
+
+  /* Update book */
+  UpdateBook(id: string, book: Book) {
+    this.bookRef
+      .update({
+        book_name: book.book_name,
+        isbn_10: book.isbn_10,
+        author_name: book.author_name,
+        publication_date: book.publication_date,
+        binding_type: book.binding_type,
+        in_stock: book.in_stock,
+        languages: book.languages,
+      })
+      .catch((error) => {
+        this.errorMgt(error);
+      });
+  }
+
+  /* Delete book */
+  DeleteBook(id: string) {
+    this.bookRef = this.db.object('books-list/' + id);
+    this.bookRef.remove().catch((error) => {
+      this.errorMgt(error);
+    });
+  }
+
+  // Error management
+  private errorMgt(error: any) {
+    console.log(error);
+  }
+}
