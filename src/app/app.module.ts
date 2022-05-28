e4e3c6bc48771b81909606d850eee0814228043b
washington-shoji <shoji.washington@gmail.com>
2022-05-28 16:08:36 +1000

    refactor(module): Added browser animation module to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,12 @@ import { AngularMaterialModule } from './material/material.module';
     EditBookComponent,
     BookListComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, AngularMaterialModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
